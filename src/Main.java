9800ee5ac328ab17f7b43e83968a872998741f28
Raktbastr <raktbastr@halfheart.net>
2024-08-06 21:09:00 -0700

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
+    public static void getuser(String[] args) {
+        String username = System.getProperty("user.name");
+    }
+
     public static void main(String[] args) throws IOException {
         String osName = System.getProperty("os.name");
         if (osName.contains("Mac")) {
@@ -15,6 +19,8 @@ public class Main {
             File file3 = new File("~/Documents/raes-files/System32.txt");
             File file4 = new File("~/Documents/raes-files/picture.txt");
             Files.createDirectories(Paths.get("~/Documents/raes-files/pookie-pics"));
+            MACRunner.main();
+
         }
         if (osName.contains("Linux")) {
             Files.createDirectories(Paths.get("~/Documents/raes-files"));
@@ -23,41 +29,38 @@ public class Main {
             File file3 = new File("~/Documents/raes-files/System32.txt");
             File file4 = new File("~/Documents/raes-files/picture.jpg");
             Files.createDirectories(Paths.get("~/Documents/raes-files/pookie-pics"));
+            LinuxRunner.main();
         }
         if (osName.contains("Windows")) {
-            String username = System.getProperty("user.name");
             Files.createDirectories(Paths.get("C:\\Users\\" + username + "\\Documents\\raes-files"));
             File file1 = new File("C:\\Users\\" + username + "\\Documents\\raes-files\\plottodestroyandrewlalko.txt");
             File file2 = new File("C:\\Users\\" + username + "\\Documents\\raes-files\\howtoSTEALpookie.txt");
             File file3 = new File("C:\\Users\\" + username + "\\Documents\\raes-files\\System32.txt");
-            File file4 = new File("C:\\Users\\" + username + "\\Documents\\raes-files\\picture.jpg");
+            File file4 = new File("C:\\Users\\" +  + "\\Documents\\raes-files\\picture.jpg");
             Files.createDirectories(Paths.get("C:\\Users\\" + username + "\\Documents\\raes-files\\pookie-pics"));
+            CMDRunner.main();
         }
     }
-}
-
-class delfiles {
-    private static void def(String[] args) {
-
+    static void main() {
+        ProcessBuilder winpb = new ProcessBuilder();
+        winpb.command("cmd.exe", "/c", "start C:\\Users\\" +  + "\\Documents\\raes-files");
     }
 }
 
 class CMDRunner {
-    static void main(String[] args) {
+    static void main() {
         ProcessBuilder winpb = new ProcessBuilder();
-
-        String username = System.getProperty("user.name");
         winpb.command("cmd.exe", "/c", "start C:\\Users\\" + username + "\\Documents\\raes-files");
     }
 }
 class MACRunner {
-    static void main(String[] args) {
+    static void main() {
         ProcessBuilder macpb = new ProcessBuilder();
         macpb.command("zsh", "-c", "open ~/Documents/raes-files");
     }
 }
 class LinuxRunner {
-    static void main(String[] args) {
+    static void main() {
         ProcessBuilder linuxpb = new ProcessBuilder();
         linuxpb.command("bash", "-c", "open ~/Documents/raes-files");
     }
